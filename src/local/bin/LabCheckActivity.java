038d17271b245fbf345c6bece9487b7180a92637
Jin <Jin@torpedo>
2011-10-04 22:54:27 -0400

    creation

---
+++ src/local/bin/LabCheckActivity.java
@@ -0,0 +1,16 @@
+package local.bin;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LabCheckActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        
+        
+    }
+}
